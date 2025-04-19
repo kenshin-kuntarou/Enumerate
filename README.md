1d54d34116d073a6f9ab5df6d15db86936aa8824
Kuntarou <kenshin_kuntarou@protonmail.com>
2025-04-19 17:48:11 -0300

    .

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# Enumerate
-  A simple directory enumeration tool
+## Enumerate Tool
+
+Essa foi uma ferramenta simples que eu desenvolvi em Python para 
+que se possa fazer uma enumeração de sub diretórios rapidamente.
+
+Ele vem com algumas wordlists que você pode usar para fazer as suas
+enumerações. Entretanto, se você desejar usar outras wordlists com a 
+ferramenta, você pode fazer digitando "`wordlists`"  no seu kali-linux
+
+--- 
+
+### Instalação
+
+O comando abaixo poderá ser usado para instalar facilmente a ferramenta.
+Se deseja você pode alterar o caminho da instalação como desejar
+
+```
+git clone https://www.github.com/kenshin-kuntarou/Enumerate ~/Downloads/
+```
+---
+
+### Argumentos
+
+Aqui eu estarei listando todos os argumentos que você poderá usar
+na linha de comando do seu terminal para executar a ferramenta.
+
+`-url`: Aqui você irá passar a url do domínio (obrigatório)
+
+```
+python3 enumerate.py -url 'https://exemplo.com/' 
+```
+
+`-file`: Aqui vpcê irá passar o diretório da wordlist (obrigatório)
+
+```
+python3 enumerate.py -file 'wordlists/test.txt'
+```
+
+`-threads`: Aqui você irá passar o número de threads que deseja usar
+
+```
+python3 enumerate.py -threads 20
+```
+
+---
---
+++ enumerate.py
@@ -0,0 +1,55 @@
+import argparse
+import requests 
+from concurrent.futures import ThreadPoolExecutor
+
+class Enumeration:
+    def __init__ (self, url_name, file_name):
+        self.url = url_name
+        self.file = file_name
+        self.result = list()
+        self.ascii_art = f"""
+-----------------------------------------------------------------
+ _______                                         __              
+|    ___|.-----.--.--.--------.-----.----.---.-.|  |_.-----.----.
+|    ___||     |  |  |        |  -__|   _|  _  ||   _|  _  |   _|
+|_______||__|__|_____|__|__|__|_____|__| |___._||____|_____|__|  
+
+------------------------------------------------------------------
+| Url: {self.url} | File: {self.file}                            
+------------------------------------------------------------------"""
+
+    def file_reader(self):
+        try:
+            with open(self.file, encoding="utf-8", errors="ignore") as f:
+                self.result = [line.strip() for line in f]
+
+        except IndexError:
+            print("| Não foi possível ler o arquivo ou ele não exisite.")
+
+    def url_request(self, numerator):
+        try: 
+            request = requests.get(f"{self.url}/{numerator}")
+
+            if request.status_code == 200:
+                print(f"| {self.url}{numerator} - status: {request.status_code}")
+            else:
+                pass
+
+        except ConnectionRefusedError:
+            print("| Erro de conexão")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-url', type=str, required=True, help="A url que será enumerada")
+    parser.add_argument('-file', type=str, required=True, help="O arquivo de enumeração")
+    parser.add_argument('-threads', type=int, required=False, help="O número de threads usada (padrão = 5)")
+    args = parser.parse_args()
+    
+    enumerator = Enumeration(args.url, args.file) 
+    print(enumerator.ascii_art)
+    enumerator.file_reader()
+
+    with ThreadPoolExecutor(max_workers=args.threads) as executor:
+        results = executor.map(enumerator.url_request, enumerator.result)
+    print("------------------------------------------------------------------")
